4159f89036db7a23c833a14f4568fab123f259d1
Jayant Sharma <sharma.jayant40@gmail.com>
2020-01-10 22:03:12 +0530

    ADDED support for online database(MONGODB)

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose')
 const validator=require('validator')
-conUrl='mongodb://127.0.0.1:27017/hackNITJ'
+conUrl='mongodb://hackNITJ:jayant.nitj@cluster0-shard-00-00-22pa2.mongodb.net:27017,cluster0-shard-00-01-22pa2.mongodb.net:27017,cluster0-shard-00-02-22pa2.mongodb.net:27017/hackNITJ?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 mongoose.connect(conUrl,{
     useNewUrlParser:true,
     useCreateIndex:true,
